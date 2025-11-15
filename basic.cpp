d2d8c2a7cfdf5cc391eadbf3c1d7c308eae185ff
ramakrishna-18 <ramanjaneyuluj14@gmail.com>
2025-11-15 07:46:53 +0530

    bhsjbc

---
+++ basic.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void find_even(vector<int>nums){
+ 
+    for(int i=0;i<nums.size();i++){
+        if(nums[i]%2==0){
+            nums[i]=-1;
+        }
+    }
+   
+}
+
+
+int main(){
+ 
+    vector<int>vec={6,77,432,67,-36,2,67,1000};
+    find_even(vec);
+   for(int i: vec){
+    cout<<i<< " ";
+   }
+    
+   return 0;
+
+}
+
+
+
+
