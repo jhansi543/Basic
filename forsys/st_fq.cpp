5a368f1c85809d1ec6c67976dd217c0694dd14d9
ramakrishna-18 <ramanjaneyuluj14@gmail.com>
2025-11-23 23:06:29 +0530

    knbdjb

---
+++ forsys/st_fq.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s="hjlnir";
+    vector<int>vec(25,0);
+    for(int i=0;i<s.length();i++){
+        vec[s[i]-'a']++;
+    }
+   
+
+    for(int i=0;i<26;i++){
+        cout<<vec[i]<<" ";
+    }
+
+}
