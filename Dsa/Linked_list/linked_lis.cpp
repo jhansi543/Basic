96f9c654c9fcb797d557c9dba10ff3be99c323c8
ramakrishna-18 <ramanjaneyuluj14@gmail.com>
2025-11-26 10:03:45 +0530

    link

--- Dsa/Linked_list/linked_lis.cpp
+++ Dsa/Linked_list/linked_lis.cpp
@@ -1,25 +1,104 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 struct node{
     int data;
-    node*next;           
-    node(int val){        
-        data=val;
+    node*next;
+    node(int v){
+        data=v;
         next=nullptr;
     }
 };
 
-int main(){
-    node*head=new node(5);
-    head->next=new node(6);   
-    head->next->next=new node(7);
-    head->next->next->next=new node(8);
-
-    node* temp=head;
-    while(temp->next!=nullptr){
-        cout<<temp->data<<"->";
-        temp=temp->next;
+void add_front(node*&head,int v){
+    node*newnode=new node(v);
+    if(!head){
+        head=newnode;
+        return ;
     }
-    cout<<temp->data;
+    node*t=head;
+    head=newnode;
+    head->next=t;
+}
+
+void desplay(node*head){
+    node*t=head;
+    while(t->next){
+        cout<<t->data<<"->";
+        t=t->next;
+    }
+    cout<<t->data<<"\n";
+}
+
+void add_back(node*&head,int v){
+    node*newnode=new node(v);
+    if(!head){
+        head=newnode;
+        return ;
+    }
+    node*t=head;
+    while(t->next){
+        t=t->next;
+    }
+    t->next=newnode;
+
+}
+
+// to find the given data is present in the linked list
+node* find_ele(node*&head,int target){
+    node*t=head;
+    while(t->next){
+        if(t->data==target){
+            return t;
+        }
+        t=t->next;
+    }
+    if(t->data==target)return t;
+    return nullptr;
+}
+// to find the given node is present in linked list
+int find_data(node*&head,node*target){
+    node*t=head;
+    while(t->next){
+        if(t==target){
+            return t->data;
+        }
+        t=t->next;
+    }
+    if(t==target)return t->data;
+    return -1;
+}
+
+int main(){
+    node*head1=nullptr;
+    node*head2=nullptr;
+    vector<int>arr={5,6,7,8,9};
+    for(int i=0;i<arr.size();i++){
+        int val=arr[i];
+        add_front(head1,val);
+    }
+    for(int i=0;i<arr.size();i++){
+        int val=arr[i];
+        add_back(head2,val);
+    }
+
+
+    desplay(head1);
+    desplay(head2);
+
+    node*x=find_ele(head1,7);
+    if(x==nullptr){
+        cout<<"ele not found";
+    }
+    else{
+        cout<<x<<" data:"<<x->data;
+    }
+    cout<<"\n";
+    node* target=head1->next->next->next;
+    int y=find_data(head1,target);
+    if(y==-1){
+        cout<<"not found";
+    }
+    else{
+        cout<<y;
+    }
 }
---
+++ Dsa/Linked_list/tempCodeRunnerFile.cpp
@@ -0,0 +1,1 @@
+addnode(head,v);
